927f1fb07595d9a3dc096661017af5954fd303ec
chimaje <chimaje.agada@pau.edu.ng>
2024-01-24 13:10:33 +0100

    assignment

---
+++ hms-apps/api-gateway/src/app.module.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EmployeesModule } from './employees/employees.module';
+import { PatientsModule } from './patients/patients.module';
+import { ClinicsModule } from './clinics/clinics.module';
+import { PharmaciesModule } from './pharmacies/pharmacies.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import{ServeStaticModule} from '@nestjs/serve-static';
+import { join } from 'path';
+
+
+@Module({
+  imports: [EmployeesModule, PatientsModule, ClinicsModule, PharmaciesModule, AuthModule, UsersModule,
+    ServeStaticModule.forRoot({
+      rootPath:join(__dirname,'../../..','users-demo-frontend','dist'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
